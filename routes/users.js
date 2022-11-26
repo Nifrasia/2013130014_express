d0b3a97595a2b4a95b35d7d4182a9e55e5d6cace
Kritsakorn Dechalert <de.kritsakorn_st@tni.ac.th>
2022-11-26 11:25:03 +0700

    Workshop A w3

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//   res.send('Hello Kitty');
+  res.status(200).json({
+    fullname: 'Pronkamol Sathiwawong'
+  });
 });
+
+router.get('/bio', function(req, res, next) {
+    res.status(200).json({
+      fullname: 'Pronkamol Sathiwawong',
+      nickname: 'Toung',
+      hobby: 'reading, gaming',
+      gitusername: 'Nifrasia'
+    });
+  });
+  
 
 module.exports = router;
