b5152cd6611814e4278d2f4aad6d42e7f0fc510a
Nifrasia <sa.pronkamol_st@tni.ac.th>
2022-12-10 11:25:07 +0700

    ConectDB

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var usersRouter = require('./routes/company');
 
 var app = express();
+
+mongoose.connect('mongodb+srv://superdev:2013210766tp@2013130014-toung.f0kvlt2.mongodb.net/restfulapi?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(logger('dev'));
 app.use(express.json());
--- controllers/comController.js
+++ controllers/comController.js
@@ -1,30 +1,37 @@
-exports.company = (req, res, next) => {
+const Company = require('../models/company')
+
+exports.company = async (req, res, next) => {
+
+    const company = await Company.findOne()
+
     res.status(200).json({
-      data: [
-        {
-            id: 1,
-            name: "Crytek",
-            address: {
-                province: "Frankfurt",
-                postcode: 60310
-            }
-        },
-        {
-            id: 2,
-            name: "Riot",
-            address: {
-                province:"Los Angeles",
-                postcode: 90005
-            }
-        },
-        {
-            id: 3,
-            name: "Mihoyo",
-            address: {
-                province:"Shanghai",
-                postcode: 200010
-            }
-        }
-      ]
+        data: company
     });
-  }
+}
+    //   data: [
+    //     {
+    //         id: 1,
+    //         name: "Crytek",
+    //         address: {
+    //             province: "Frankfurt",
+    //             postcode: 60310
+    //         }
+    //     },
+    //     {
+    //         id: 2,
+    //         name: "Riot",
+    //         address: {
+    //             province:"Los Angeles",
+    //             postcode: 90005
+    //         }
+    //     },
+    //     {
+    //         id: 3,
+    //         name: "Mihoyo",
+    //         address: {
+    //             province:"Shanghai",
+    //             postcode: 200010
+    //         }
+    //     }
+    //   ]
+   
