dd20fd5165e423b3862faf2c33b0f6ee05843ec5
Nifrasia <sa.pronkamol_st@tni.ac.th>
2022-12-24 09:52:38 +0700

    WorkshopA Edited 1

--- routes/shop.js
+++ routes/shop.js
@@ -2,5 +2,6 @@ var express = require('express');
 var router = express.Router();
 const shopController = require('../controllers/shopController')
 
+router.get('/', shopController.shop);
   
 module.exports = router;
